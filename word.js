66580a35062ccf40a8254af0298cfd044f29d6c4
jcqlng <jcqlngeiger@outlook.com>
2020-06-18 19:05:56 -0400

    Line 6, 10- Changed word Line 16,17 - Corrected the array of letter objects

--- word.js
+++ word.js
@@ -1,27 +1,58 @@
 // word constructor that uses the letter.js file to create input data 
 var Letter = require("./letter");
+const { connectableObservableDescriptor } = require("rxjs/internal/observable/ConnectableObservable");
 // function to pull and display answers through word arrays
 function Word(wordArr) {
     this.wordArr = wordArr;
-    this.testWord = [];
+    this.letters = [];
+    
+
     this.makeWord = function(){
         for (var i = 0; i < wordArr.length; i++) {
             var let = new Letter(wordArr[i]);
-            this.testWord.push(let);
+            this.letters.push(let);
         }
     }
     // Displays the word
     this.showWord = function() {
         var wordDisplay = [];
-        for (var i = 0; i < testWord.length; i++) {
-            wordDisplay.push(this.testWord[i].displayLet());
+        
+        for (var i = 0; i < this.letters.length; i++) {
+            wordDisplay.push(this.letters[i].displayLet());
+            
         }
+        
+        return wordDisplay.join(' ');
     }
+
+    //Equality comparer 
+    this.equals = function(word) {
+
+        return this.toString() === word;
+    }
+
+    //returns a clean string of the underlying letters, forming a word
+    this.toString = function() {
+        var word = [];
+
+
+        for(var i = 0; i < this.letters.length; i++) {
+            word.push(this.letters[i].displayLet());
+        }
+
+        return word.join('');
+    }
+
+    //function that loops through the letters to see if there is a match
+    //for each letter in the letters of the word.
     this.checkGuess = function(userGuess) {
-        for (let i = 0; i < this.testWord.length; i++) {
-            this.testWord[i].check(userGuess);
+        
+        for (let i = 0; i < this.letters.length; i++) {
+            this.letters[i].check(userGuess);
         }
     }
+
+    
 }
 // Node module to export the correct word
 module.exports = Word
